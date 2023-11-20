92f5a37f06a63ef1aec7ba935124dd9d8cc72229
Houssem <houssem@3dverse.com>
2023-11-20 17:14:28 -0500

    feat: demo

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
-export const publicToken = %YOUR_PUBLIC_TOKEN%;
-export const mainSceneUUID = %YOUR_MAIN_SCENE_UUID%;
-export const characterControllerSceneUUID = %YOUR_CHARACTER_CONTROLLER_SCENE_UUID%;
+export const publicToken = "public_ZfIxTOzgSYnoHzWh";
+export const mainSceneUUID = "db2319f4-9f77-4215-93b9-59e7ac101324";
+export const characterControllerSceneUUID =
+  "eb7d4a9e-1354-41ba-abbe-a9529894b84b";
